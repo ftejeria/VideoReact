341d9b750f2715278b853bab6ef0d46d04a07065
ftejeria <tejeria.felipe@gmail.com>
2022-03-21 21:48:05 -0300

    first commit

---
+++ src/apis/Youtube.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const KEY = "AIzaSyAgBB7md6rxuWNgsqYNE48yuVpP0YfeL8k";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: 'snippet', 
+    type: 'video',
+    maxResults: 5,
+    key: KEY
+  },
+});
---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import Youtube from "../apis/Youtube";
+import VideoList from "./VideoList";
+
+class App extends React.Component {
+  state = { videos: [] };
+  onTermSubmit = async (term) => {
+    const response = await Youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+    this.setState({ videos: response.data.items });
+  };
+
+  render() {
+    return (
+      <div>
+
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+export default App;
